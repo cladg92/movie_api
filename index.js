333d2953d1932da60fb15d5b870c8efded9f44cd
cladg92 <cladg92@gmail.com>
2022-04-27 16:57:02 +0200

    set express.static and API endpoints

--- index.js
+++ index.js
@@ -8,15 +8,35 @@ const app = express();
 // a ‘log.txt’ file is created in root directory
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
+let Top10Movies = [
+    {id: 1, title: "title1"},
+    {id: 2, title: "title2"},
+    {id: 3, title: "title3"},
+    {id: 4, title: "title3"},
+    {id: 5, title: "title5"},
+    {id: 6, title: "title6"},
+    {id: 7, title: "title7"},
+    {id: 8, title: "title8"},
+    {id: 9, title: "title9"},
+    {id: 10, title: "title9"},
+]
+
 // setup the logger
 app.use(morgan('combined', {stream: accessLogStream}));
 
+// setup static
+app.use(express.static('public'));
+
+
+
+
+//setting endpoints for API
 app.get('/', (req, res) => {
   res.send('Welcome to my app!');
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+app.get('/movies', (req, res) => {
+  res.json(Top10Movies);
 });
 
 app.listen(8080, () => {
