5fba7dbcad468b205141eed99e72d93e105dfcb7
cladg92 <cladg92@gmail.com>
2022-05-15 16:41:49 +0200

    Allow all domains

--- index.js
+++ index.js
@@ -21,18 +21,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const {check, validationResult} = require('express-validator'); // for user input validation
 const cors = require('cors');
 // Define allowed domains (origins) for cross-origin resource sharing (CORS)
-let allowedOrigins = ['http://localhost:8080', 'https://myflixapi92.herokuapp.com/'];
+//let allowedOrigins = ['http://localhost:8080', 'https://myflixapi92.herokuapp.com/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 let auth = require('./auth')(app);
 const passport = require('passport');
